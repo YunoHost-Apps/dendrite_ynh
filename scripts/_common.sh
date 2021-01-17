0d326165a769cc75aa2c0f9893cc6f24d06dde96
tituspijean <tituspijean@outlook.com>
2021-01-17 16:42:19 +0100

    Not using pkg_dependencies yet

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="golang-go"
+#pkg_dependencies="golang-go"
 
 #=================================================
 # PERSONAL HELPERS
