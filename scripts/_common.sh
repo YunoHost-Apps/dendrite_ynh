6f073ae0864c73f38e34ae4b00d2d7f509cfe0f7
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-02-19 22:21:29 +0100

    Bump Golang to 1.20

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="postgresql postgresql-contrib"
 
-GO_VERSION="1.18"
+GO_VERSION="1.20"
 psql_version=$PSQL_VERSION
 
 #=================================================
