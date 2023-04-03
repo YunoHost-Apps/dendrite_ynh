a5556e08071be0bf2eb9d7cadc241fc2f0c319d7
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-03 22:20:38 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="postgresql postgresql-contrib"
-
 GO_VERSION="1.20"
 psql_version=$PSQL_VERSION
 
