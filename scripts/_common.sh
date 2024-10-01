7c1e3e9d4d7f19cd53538338638df245dead7844
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-10-01 08:23:56 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,5 +3,6 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
 go_version="1.21"
 psql_version=$PSQL_VERSION
